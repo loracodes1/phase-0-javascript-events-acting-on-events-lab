b51d4ecf0aedf770b8a55b4e20bdbd0d668cf2d6
florence <loracodes3@gmail.com>
2024-10-18 12:42:38 +0300

    move dodger to right

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ function moveDodgerLeft() {
     }
   }
   
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left.replace('px', ''), 10);
+  
+    if (left < 360) { 
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
   
