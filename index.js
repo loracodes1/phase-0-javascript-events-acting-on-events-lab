c5e79bd1f53da2611c9fbf8a69097392c344b96d
florence <loracodes3@gmail.com>
2024-10-18 12:41:31 +0300

    moving dodger to left

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left.replace('px', ''), 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  }
+  
+  
